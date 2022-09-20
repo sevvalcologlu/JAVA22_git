bad57bc16a6044560aba52cf1d04f8e45e43b327
sevval <sevval@71db.wls.metu.edu.tr>
2022-09-20 14:52:38 +0300

    3

---
+++ datatypes/src/datatypes/Main.java
@@ -0,0 +1,15 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double sayı = 12.5 ;
+		
+		String şehir = "Ankara";
+		char karakter = 'A';
+		boolean dogruMu = false;
+
+
+	}
+
+}
