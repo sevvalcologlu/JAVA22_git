1074a539c4d7fe5ad1450be08a218fd2f3a1c5e9
sevval <sevval@71db.wls.metu.edu.tr>
2022-09-20 13:53:52 +0300

    ok

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int öğrenciSayısı = 10;
+		
+		System.out.println("Öğrenci sayım: "+ öğrenciSayısı);
+		System.out.println("Öğrenci sayım: 10");
+		System.out.println("Öğrenci sayım: 10");
+		System.out.println("Öğrenci sayım: 10");
+
+	}
+
+}
