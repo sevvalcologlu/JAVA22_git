0c956850f5c8d9a6cd036f49dd5e5171ad4b55a3
sevval <sevval@71db.wls.metu.edu.tr>
2022-09-20 15:49:16 +0300

    4

---
+++ conditions/src/conditions/Main.java
@@ -0,0 +1,21 @@
+package conditions;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi =24;
+		if (sayi<20){
+			System.out.println("Sayı 20 den küçüktür");
+			
+			//if (sayi<15){
+				//System.out.println("Sayı 15 den küçüktür");
+			
+		}else if (sayi==20){
+			System.out.println("Sayı 20 den küçüktür değildir");
+		}else{
+			System.out.println("Sayı 20 den büyüktür");
+			
+		}
+	}
+
+}
