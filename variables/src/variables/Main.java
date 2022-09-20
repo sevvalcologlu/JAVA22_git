2fa504baeb1df58611b1a5fab5524fbc8b0ce227
sevval <sevval@71db.wls.metu.edu.tr>
2022-09-20 14:04:02 +0300

    2

--- variables/src/variables/Main.java
+++ variables/src/variables/Main.java
@@ -5,12 +5,16 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int öğrenciSayısı = 10;
+		//reusability
 		
-		System.out.println("Öğrenci sayım: "+ öğrenciSayısı);
-		System.out.println("Öğrenci sayım: 10");
-		System.out.println("Öğrenci sayım: 10");
-		System.out.println("Öğrenci sayım: 10");
+		int öğrenciSayısı = 12;
+		String mesaj = "Öğrenci Sayısı :";
+		
+		System.out.println(mesaj + öğrenciSayısı);
+		System.out.println(mesaj + öğrenciSayısı);
+		System.out.println(mesaj + öğrenciSayısı);
+		System.out.println(mesaj + öğrenciSayısı);
+		
 
 	}
 
